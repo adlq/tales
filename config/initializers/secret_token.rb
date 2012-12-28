820f936a1534ae67118baaaa2a33d17a4c6bbcaf
adlq <dtnghia90@gmail.com>
2012-12-28 20:32:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tales::Application.config.secret_token = '0402fbbbe8b4f79c91719d6a2a7b633cbc2c291909936e18961a822366053239a6482bc000b2c8aeff1dea4f223bdb5d2b96f615c22c54945d81307282f54394'
