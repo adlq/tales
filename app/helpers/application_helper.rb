ea0c4eadd49edca1fcacf5822da946474fcd31c8
adlq <dtnghia90@gmail.com>
2012-12-29 12:09:19 +0100

    Add bootstrap and Markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
       "#{base_title} - #{title}"
     end
   end
+
+  # Conversion from Markdown to HTML using RedCarpet
+  def markdown_format(text)
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true
+    }
+    renderer = Redcarpet::Render::HTML
+    markdown = Redcarpet::Markdown.new(renderer, options)
+    markdown.render(text).html_safe
+  end
 end
